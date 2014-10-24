8676dc03ff125dcfdf2d9d698d03f4ea08605135
Alexander De Leon <me@alexdeleon.name>
2014-10-24 18:31:54 +0200

    Bumped version to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ name              "chef_solo_helper"
 maintainer        "Alexander De Leon"
 maintainer_email  "me@alexdeleon.name"
 description       "Chef solo helper recipes"
-version           "0.1.0"
+version           "0.1.1"
 
 recipe "chef_solo_cron",      "Installs cron job to periodically reprovision with chef-solo"
