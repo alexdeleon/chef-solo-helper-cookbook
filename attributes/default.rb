039df3e6083b6af4b7abcfca9c95ac3fe4f61e56
Alexander De Leon <me@alexdeleon.name>
2014-10-24 18:31:35 +0200

    Update default installation of chef-solo

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default[:chef_solo][:bin]          = 'chef-solo'
+default[:chef_solo][:bin]          = '/opt/chef/bin/chef-solo'
 default[:chef_solo][:data_file]    = '/srv/vagrant-chef/dna.json'
 default[:chef_solo][:config_file]  = '/srv/vagrant-chef/solo.rb'
 default[:chef_solo][:environment_variables] = ''
